9ca33afff0c23cb46dc4aae5ac80e99e34fd650c
Edison Chen <echen2357@gmail.com>
2023-01-08 20:09:44 -0800

    isAnagram

--- neetcode-150.py
+++ neetcode-150.py
@@ -8,4 +8,17 @@ def containsDuplicate(nums):
     return False
 
 
-assert containsDuplicate([1, 2, 3, 1]) is True
+# 2: https://leetcode.com/problems/valid-anagram/description/
+def isAnagram(self, s, t):
+    count_s, count_t = {}, {}
+    if len(s) != len(t):
+        return False
+    for i in range(len(s)):
+        count_s[s[i]] = count_s.get(s[i], 0) + 1
+        count_t[t[i]] = count_t.get(t[i], 0) + 1
+
+    for c in count_s:
+        if count_s[c] != count_t.get(c, 0):
+            return False
+    return True
+
