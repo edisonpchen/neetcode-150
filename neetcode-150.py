d4f596ae445b2bb6f837f89e06f30917a76a5688
Edison Chen <echen2357@gmail.com>
2023-01-15 14:57:03 -0800

    best time to buy and sell stock

--- neetcode-150.py
+++ neetcode-150.py
@@ -90,4 +90,17 @@ def maxArea(self, height) -> int:
             right -= 1
 
     return currMax
-    
+
+
+# https://leetcode.com/problems/best-time-to-buy-and-sell-stock
+def maxProfit(self, prices: List[int]) -> int:
+    l, r = 0, 1
+    maxProfit = 0
+    while r < len(prices):
+        if prices[l] < prices[r]:
+            profit = prices[r] - prices[l]
+            maxProfit = max(maxProfit, profit)
+        else:
+            l = r
+        r += 1
+    return maxProfit
